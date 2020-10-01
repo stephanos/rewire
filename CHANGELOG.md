1c0578983cff1a79b4d88e88922ea5c51a4d7d5c
Stephan Behnke <github@stephanbehnke.de>
2020-09-30 18:37:33 -0700

    release 0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## [0.1.1] - 2020-09-30
+
+### Changed
+* fixed deprecation warning in Elixir 1.10+
+
+
 ## [0.1.0] - 2020-09-28
 
 ### Added
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Rewire.MixProject do
   def project do
     [
       app: :rewire,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
