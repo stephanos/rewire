1c26f85ea3b2fc2ff7b6fe22fa8ee4718e4ef7a9
Stephan Behnke <github@stephanbehnke.de>
2020-10-12 18:04:33 -0700

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Rewire.MixProject do
   def project do
     [
       app: :rewire,
-      version: "0.4.0",
+      version: "0.4.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
