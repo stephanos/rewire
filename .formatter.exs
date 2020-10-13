09316328798687c22cea483b5ad79a971c91655d
Stephan Behnke <github@stephanbehnke.de>
2020-10-12 18:08:06 -0700

    add CI

--- .formatter.exs
+++ .formatter.exs
@@ -1,7 +1,12 @@
-# Used by "mix format"
+locals_without_parens = [
+  rewire: 2,
+  rewire: 3
+]
+
 [
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  locals_without_parens: locals_without_parens,
   export: [
-    locals_without_parens: [rewire: 2, rewire: 3]
+    locals_without_parens: locals_without_parens
   ]
 ]
