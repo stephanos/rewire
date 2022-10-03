aaac378be8fd038b88ed367abd28924f9ead20c8
Stephan Behnke <stephanos@users.noreply.github.com>
2022-10-02 17:02:25 -0700

    fix

--- lib/rewire/module.ex
+++ lib/rewire/module.ex
@@ -285,6 +285,6 @@ defmodule Rewire.Module do
   defp flatten({:__block__, [], [[], {:defmodule, _, _} = nested, []]}) do
     {:__block__, [], [nested]}
   end
-  
+
   defp flatten(ast), do: ast
 end
